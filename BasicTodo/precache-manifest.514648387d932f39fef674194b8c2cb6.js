10e6e7d04e6226cf671b5b2cdb97cd22b8e2e446
sandeeprvarma <sandeepkvarma20@gmail.com>
2020-04-14 16:58:42 +0530

    Adde basic todo app

---
+++ BasicTodo/precache-manifest.514648387d932f39fef674194b8c2cb6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "536c549e859953791db2c2eedc2d46c3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfc5a02a7268e8acf452",
+    "url": "/static/css/main.0ddf145d.chunk.css"
+  },
+  {
+    "revision": "ab72e67e93b981305dfd",
+    "url": "/static/js/2.06c19259.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.06c19259.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfc5a02a7268e8acf452",
+    "url": "/static/js/main.292e285d.chunk.js"
+  },
+  {
+    "revision": "bcb3e891f8c02b6d3b30",
+    "url": "/static/js/runtime-main.2d5c5ce2.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
